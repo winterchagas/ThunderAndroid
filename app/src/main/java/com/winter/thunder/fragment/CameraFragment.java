e24453318c84f120bf546f13913da1ac3c39ca5f
winterchagas <winterchagas@gmail.com>
2017-07-08 15:40:16 -0700

    camera_frag

---
+++ app/src/main/java/com/winter/thunder/fragment/CameraFragment.java
@@ -0,0 +1,26 @@
+package com.winter.thunder.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import com.winter.thunder.R;
+
+public class CameraFragment extends Fragment {
+
+    public CameraFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_camera, container, false);
+        return v;
+    }
+
+}
